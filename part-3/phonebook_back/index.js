852a34328b2589d7d21cb1d8f4b603f6ca9ac9bb
Gabriel <gszcode@gmail.com>
2023-10-10 02:54:52 -0300

    nodejs and express - exercises from 3.1 to 3.8

---
+++ part-3/phonebook_back/index.js
@@ -0,0 +1,91 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const PORT = 3001
+
+let persons = [
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '123-321-1231'
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '425-123-1234'
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '905-175-4264'
+  },
+  {
+    id: 4,
+    name: 'Mary Poppendick',
+    number: '381-053-3953'
+  }
+]
+
+app.use(express.json())
+app.use(morgan('dev'))
+
+let accessCount = 0
+app.get('/api/persons', (req, res) => {
+  accessCount += 1
+
+  return res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+  const hour = new Date()
+
+  res.send(
+    `<h3>Phonebook has info for ${accessCount} people<h3/> <br/> <p>${hour}<p/>`
+  )
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const { id } = req.params
+  const person = persons.find((person) => person.id === Number(id))
+
+  if (!person) return res.status(404).end()
+
+  res.json(person)
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const { id } = req.params
+  const person = persons.filter((person) => person.id !== Number(id))
+
+  if (!person) return res.status(404)
+
+  res.json(person)
+})
+
+app.post('/api/persons', (req, res) => {
+  const { name, number } = req.body
+  const person = persons.find((person) => person.name === name)
+
+  if (!name || !number) return res.status(400).json({ error: 'data missing' })
+  if (person) return res.status(400).json({ error: 'name must be unique' })
+
+  const newPerson = {
+    id: Math.round(Math.random() * 1000),
+    name,
+    number
+  }
+
+  persons = persons.concat(newPerson)
+
+  res.json(persons)
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+app.listen(PORT, () => {
+  console.log(`Server in Port: ${PORT}`)
+})
---
+++ part-3/study/index.js
@@ -0,0 +1,79 @@
+const express = require('express')
+const app = express()
+const PORT = 3001
+
+let notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    date: '2019-05-30T17:30:31.098Z',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only Javascript',
+    date: '2019-05-30T18:39:34.091Z',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2019-05-30T19:20:14.298Z',
+    important: true
+  }
+]
+
+app.use(express.json())
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
+app.get('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const note = notes.find((note) => note.id === id)
+
+  if (!note) res.status(404).end()
+
+  res.json(note)
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const note = notes.filter((note) => note.id !== id)
+
+  if (!note) res.status(404).end()
+
+  res.json(204).end()
+})
+
+const generateId = () => {
+  const maxId = notes.length ? Math.max(...notes.map((note) => note.id)) : 0
+
+  return maxId + 1
+}
+
+app.post('/api/notes', (req, res) => {
+  const { content, important } = req.body
+
+  if (!content) return res.status(400).json({ error: 'content missing' })
+
+  const note = {
+    id: generateId(),
+    content,
+    important: important || false,
+    date: new Date()
+  }
+
+  notes = [...notes, note]
+
+  res.status(200).json(notes)
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on PORT: ${PORT}`)
+})
